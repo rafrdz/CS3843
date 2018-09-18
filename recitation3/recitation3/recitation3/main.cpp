bd2acd1bc1e0752601b01c506c7ff1ef27d1499a
Rafael Rodriguez <rrodriguez1989@gmail.com>
2018-09-17 21:31:38 -0500

    Finished work on recitation3

--- recitation3/recitation3/recitation3/main.cpp
+++ recitation3/recitation3/recitation3/main.cpp
@@ -1,3 +1,38 @@
 // Recitation 3
 // By: Rafael Rodriguez - mat574
 
+#include <Windows.h>
+#include <stdio.h>
+
+unsigned char gArray[] = { 0x05, 0xFE, 0x1A, 0x1C, 0x28, 0xCC, 0xD3, 0x81 };
+short int gArraySI[] = { 0x05, 0xFE, 0x1A, 0x1C, 0x28, 0xCC, 0xD3, 0x81 };
+int gArrayI[] = { 0x05, 0xFE, 0x1A, 0x1C, 0x28, 0xCC, 0xD3, 0x81 };
+
+int test()
+{
+	__asm {
+		mov al, gArray
+		lea esi, gArray
+		mov dl, byte ptr [esi]
+		mov edx, dword ptr [esi+2]
+		mov al, gArray[5] 
+		nop
+		mov ax, gArraySI
+		lea esi, gArraySI
+		mov dl, byte ptr [esi]
+		mov edx, dword ptr [esi+2]
+		mov ax, gArraySI[5]
+		mov eax, gArrayI
+		lea esi, gArrayI
+		mov dl, byte ptr [esi]
+		mov edx, dword ptr [esi+2]
+		mov eax, gArrayI[5]
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	test();
+	exit(0);
+}
