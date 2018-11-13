062298a9ac2232e2ee1a98445f8e0dbccb11a676
Rafael Rodriguez <rrodriguez1989@gmail.com>
2018-11-12 18:47:41 -0600

    Reworked logic for "swap nibble", added comments, and formatted code

--- CS3843/CS3843/DecryptData_Initial.cpp
+++ CS3843/CS3843/DecryptData_Initial.cpp
@@ -20,94 +20,84 @@ int decryptData(char *data, int dataLength)
 	// Also, you cannot use a lot of global variables - work with registers
 
 	__asm {
-		// Encrypt code by Team 6
+		// Decrypt code by Team 6
 		// Rafael Rodriguez, David Brenner, Jacob De Hoyos
-		xor esi, esi
-			xor edi, edi
-			xor eax, eax
-			xor ebx, ebx
-			xor ecx, ecx
-			xor edx, edx
+		mov esi, gptrKey // Copy pointer to gKey to esi
+		mov edi, gptrPasswordHash // Copy pointer to gPasswordHash to edi
 
-			mov esi, gptrKey
+		movzx eax, byte ptr[edi] // Get first byte of password hash
+		shl eax, 8 // Multiply first byte of password hash by 256
+		movzx ebx, byte ptr[edi + 1] // Get second byte of password hash
+		add eax, ebx // Sum the two bytes to get starting_index
 
-			mov edi, gptrPasswordHash
+		movzx edx, byte ptr[gkey + eax] // Get the gKey[starting_index] value
+		mov esi, data // Copy data to esi
 
-			movzx eax, byte ptr[edi] // Get first byte of password hash
-			shl eax, 8 // Multiply first byte of password hash by 256
-			movzx ebx, byte ptr[edi + 1] // Get second byte of password hash
-			add eax, ebx // Sum the two bytes to get starting_index
+		xor ebx, ebx // Zero out ebx to use at the iterator value
+		xor eax, eax // Zero out eax to use as xor temp value
+		xor edi, edi // Zero out edi to use for dataLength
 
-			movzx edx, byte ptr[gkey + eax] // Get the gKey[starting_index] value
-			mov esi, data
+		mov edi, dataLength
+		dec edi // Decrement edi for zero-index
 
-			xor ebx, ebx // Zero out ebx to use at the iterator value
-			xor eax, eax // Zero out eax to use as xor temp value
-			xor edi, edi // Zero out edi to store dataLength
+	DEC_LOOP:
+		movzx ecx, byte ptr[esi + ebx] // Copy byte of data into ecx
+		xor ecx, edx // xor byte of data with gKey[starting_index]
 
-			mov edi, dataLength
-			dec edi // Decrement edi for 0-indexing
+		/*// D - Code Table Swap
+		push edi // Push "dataLength - 1" to the stack
+		lea edi, gDecodeTable // Move address of gDecodeTable to edi
+		movzx edi, byte ptr [edi + ecx]
+		mov ecx, edi
+		pop edi*/
 
-		ENC_LOOP :
-			mov cl, byte ptr[esi + ebx] // Copy byte of data into al
-			xor cl, dl // xor byte of data with gKey[starting_index]
+		// E - Reverse Bit Order
+		push ebx // Push iterator to the stack
+		mov ebx, 8 // Set the reverse loop counter
 
-			// D - Code Table Swap
-			push edi
-			lea edi, gDecodeTable
-			movzx edi, byte ptr[edi + ecx]
-			mov ecx, edi
-			pop edi
+	REVERSE:
+		shr ecx, 1
+		rcl eax, 1
+		dec ebx
+		jne REVERSE
+		mov ecx, eax
+		xor ebx, ebx
+		xor eax, eax
+		pop ebx
 
-			// E - Reverse Bit Order
-			push ebx
-			xor ebx, ebx
-			mov  bl, 8
+		// A - Swap Even/Odd Bits
+		mov eax, ecx // Copy value to eax
+		shl eax, 1 // Shift left by 1
+		and eax, 0xAA // And value by 0xAA
+		shr ecx, 1 // Shift right by 1
+		and ecx, 0x55 // And value by 0x55
+		add ecx, eax // Add values together and store in ecx
+		xor eax, eax // Zero out eax
 
-		REVERSE:
-			shr ecx, 1
-			rcl al, 1
-			dec bl
-			jne REVERSE
-			mov ecx, eax
-			xor ebx, ebx
-			xor eax, eax
-			pop ebx
+		// C - Swap Nibbles
+		push ebx // Push ebx to stack
+		xor ebx, ebx // Zero out ebx
+		mov eax, ecx // Copy value to eax
+		and eax, 0x0F // And eax with 0x0F to zero out all but last 4 bits
+		shl eax, 4 // Shift left by 4 to make the new last 4 bits 0
+		mov ebx, ecx // Copy value to ebx
+		and ebx, 0xF0 // And ebx with 0xF0 to zero out all but first 4 bits
+		shr ebx, 4 // Shift right by 4 to make the new first 4 bits 0
+		or eax, ebx // Or the two registers to swap the nibbles
+		mov ecx, eax // Copy result into value
+		xor eax, eax // Zero out eax
+		pop ebx // Pop ebx off of the stack
 
-			// A - Swap Even/Odd Bits
-			mov eax, ecx
-			shl eax, 1
-			and eax, 0xAA
-			shr ecx, 1
-			and ecx, 0x55
-			add ecx, eax
-			xor eax, eax
+		// B - Invert Middle 4 Bits
+		xor ecx, 0x3C // xor value with 0x3C to invert middle 4 bits
 
-			// C - Swap Nibbles
-			push ebx
-			xor ebx, ebx
-			mov  bl, 4
+		mov byte ptr[esi + ebx], cl // Copy cl back into data
+		cmp ebx, edi // Check if end of data
+		je DEC_EXIT // End loop and exit
+		inc ebx // Increment iterator
+		jmp DEC_LOOP // Repeat loop
 
-		REVERSE_NIBBLE:
-			shr ecx, 1
-			rcr al, 1
-			dec bl
-			jne REVERSE_NIBBLE
-			add ecx, eax
-			xor ebx, ebx
-			xor eax, eax
-			pop ebx
-
-			// B - Invert Middle 4 Bits
-			xor ecx, 0x3C
-
-			mov byte ptr[esi + ebx], cl // Copy al back into data
-			inc ebx // Increment iterator
-			cmp ebx, edi // Check if end of data
-			je ENC_EXIT // End loop and exit
-			jmp ENC_LOOP // Repeat loop
-
-		ENC_EXIT :
+	DEC_EXIT:
 	}
 	return resulti;
 } // decryptData
--- CS3843/CS3843/EncryptData_Initial.cpp
+++ CS3843/CS3843/EncryptData_Initial.cpp
@@ -20,18 +20,10 @@ int encryptData(char *data, int dataLength)
 	// Also, you cannot use a lot of global variables - work with registers
 
 	__asm {
-		// Encrypt code by Team 6
+		// Decrypt code by Team 6
 		// Rafael Rodriguez, David Brenner, Jacob De Hoyos
-		xor esi, esi
-		xor edi, edi
-		xor eax, eax
-		xor ebx, ebx
-		xor ecx, ecx
-		xor edx, edx
-
-		mov esi, gptrKey
-
-		mov edi, gptrPasswordHash
+		mov esi, gptrKey // Copy pointer to gKey to esi
+		mov edi, gptrPasswordHash // Copy pointer to gPasswordHash to edi
 
 		movzx eax, byte ptr[edi] // Get first byte of password hash
 		shl eax, 8 // Multiply first byte of password hash by 256
@@ -39,75 +31,73 @@ int encryptData(char *data, int dataLength)
 		add eax, ebx // Sum the two bytes to get starting_index
 
 		movzx edx, byte ptr[gkey + eax] // Get the gKey[starting_index] value
-		mov esi, data
+		mov esi, data // Copy data to esi
 
 		xor ebx, ebx // Zero out ebx to use at the iterator value
 		xor eax, eax // Zero out eax to use as xor temp value
-		xor edi, edi // Zero out edi to store dataLength
+		xor edi, edi // Zero out edi to use for dataLength
 
 		mov edi, dataLength
-		dec edi // Decrement edi for 0-indexing
+		dec edi // Decrement edi for zero-index
 
-	ENC_LOOP:
-		mov cl, byte ptr[esi + ebx] // Copy byte of data into al
-		xor cl, dl // xor byte of data with gKey[starting_index]
-		
-		// D - Code Table Swap
-		push edi
-		lea edi, gEncodeTable
-		movzx edi, byte ptr [edi+ecx]
+	ENC_LOOP :
+		movzx ecx, byte ptr[esi + ebx] // Copy byte of data into ecx
+		xor ecx, edx // xor byte of data with gKey[starting_index]
+
+		/*// D - Code Table Swap
+		push edi // Push "dataLength - 1" to the stack
+		lea edi, gDecodeTable // Move address of gDecodeTable to edi
+		movzx edi, byte ptr [edi + ecx]
 		mov ecx, edi
-		pop edi
-		
+		pop edi*/
+
 		// E - Reverse Bit Order
-		push ebx
-		xor ebx,ebx
-		mov  bl, 8
-	
+		push ebx // Push iterator to the stack
+		mov ebx, 8 // Set the reverse loop counter
+
 	REVERSE:
 		shr ecx, 1
-		rcl al, 1
-		dec bl
+		rcl eax, 1
+		dec ebx
 		jne REVERSE
-		mov ecx,eax
-		xor ebx,ebx
-		xor eax,eax
-		pop ebx
-		
-		// A - Swap Even/Odd Bits
-		mov eax, ecx
-		shl eax ,1
-		and eax , 0xAA
-		shr ecx , 1
-		and ecx, 0x55
-		add ecx,eax
-		xor eax,eax
-		
-		// C - Swap Nibbles
-		push ebx
-		xor ebx, ebx
-		mov  bl, 4
-	
-	REVERSE_NIBBLE:
-		shr ecx, 1
-		rcr al, 1
-		dec bl
-		jne REVERSE_NIBBLE
-		add ecx, eax
+		mov ecx, eax
 		xor ebx, ebx
 		xor eax, eax
 		pop ebx
-		
-		// B - Invert Middle 4 Bits
-		xor ecx, 0x3C
 
-		mov byte ptr[esi + ebx], cl // Copy al back into data
-		inc ebx // Increment iterator
+		// A - Swap Even/Odd Bits
+		mov eax, ecx // Copy value to eax
+		shl eax, 1 // Shift left by 1
+		and eax, 0xAA // And value by 0xAA
+		shr ecx, 1 // Shift right by 1
+		and ecx, 0x55 // And value by 0x55
+		add ecx, eax // Add values together and store in ecx
+		xor eax, eax // Zero out eax
+
+		// C - Swap Nibbles
+		push ebx // Push ebx to stack
+		xor ebx, ebx // Zero out ebx
+		mov eax, ecx // Copy value to eax
+		and eax, 0x0F // And eax with 0x0F to zero out all but last 4 bits
+		shl eax, 4 // Shift left by 4 to make the new last 4 bits 0
+		mov ebx, ecx // Copy value to ebx
+		and ebx, 0xF0 // And ebx with 0xF0 to zero out all but first 4 bits
+		shr ebx, 4 // Shift right by 4 to make the new first 4 bits 0
+		or eax, ebx // Or the two registers to swap the nibbles
+		mov ecx, eax // Copy result into value
+		xor eax, eax // Zero out eax
+		pop ebx // Pop ebx off of the stack
+
+		// B - Invert Middle 4 Bits
+		xor ecx, 0x3C // xor value with 0x3C to invert middle 4 bits
+
+		mov byte ptr[esi + ebx], cl // Copy cl back into data
 		cmp ebx, edi // Check if end of data
 		je ENC_EXIT // End loop and exit
+		inc ebx // Increment iterator
 		jmp ENC_LOOP // Repeat loop
 
-	ENC_EXIT:
+	ENC_EXIT :
 	}
 	return resulti;
 } // encryptData
